1587587c71a60f5aefcd0552e8b73275111a361c
Mohamed Eid <m0hamed.3ed98@gmail.com>
2022-03-06 15:16:23 +0200

    problem0

---
+++ C_Programming/Task2/problem0.c
@@ -0,0 +1,29 @@
+/*
+ * problem0.c
+ *
+ *  Created on: Mar 6, 2022
+ *      Author: mohamed
+ */
+
+
+#include <stdio.h>
+
+enum months
+{
+	jan=1, feb, march, april, may, june, july, augast, sept, oct, nov, dec
+}month;
+
+
+int main()
+{
+	printf("jan = %d\n", jan);
+	printf("feb = %d\n", feb);
+	printf("june = %d\n", june);
+	printf("dec = %d\n", dec);
+
+
+
+
+
+	return 0;
+}
